9c7b2ebecccec00acdf64d8897046ec082601588
William L <w-lindvall@outlook.com>
2019-05-09 15:50:08 -0700

    Fix: Remove deleted import

--- server/main.js
+++ server/main.js
@@ -1,8 +1,3 @@
 import { Meteor } from "meteor/meteor";
-import Links from "/imports/api/links";
-
-function insertLink(title, url) {
-  Links.insert({ title, url, createdAt: new Date() });
-}
 
 Meteor.startup(() => {});
