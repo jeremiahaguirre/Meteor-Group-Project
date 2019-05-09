c67082f6aef455930e9fe505aec1a9ab0d727aae
Gavin Jin <gavinjin62@gmail.com>
2019-05-09 15:36:15 -0700

    Add post job query

---
+++ imports/api/jobs.js
@@ -0,0 +1,21 @@
+import { Mongo } from "meteor/mongo";
+
+export const Jobs = new Mongo.Collection("jobs");
+
+Meteor.methods({
+  "jobs.insert"(title, description) {
+    // if (!this.userId) {
+    //   throw new Meteor.Error("not-authorized");
+    // }
+
+    Jobs.insert({
+      title,
+      description,
+      location: undefined,
+      professions: [],
+      // workspace
+      createdAt: new Date(),
+      owner: 0 // change when auth set up: (this.userId)
+    });
+  }
+});
