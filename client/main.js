feb0d6b6d3c364b41fe77dc9df1e4deeb80e5707
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-09 11:14:15 -0700

    Import app from a containers directory

--- client/main.js
+++ client/main.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import App from '/imports/ui/App'
+import App from '../imports/ui/containers/App';
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('react-target'));
+  ReactDOM.render(<App />, document.getElementById('react-target'));
 });
---
+++ imports/ui/containers/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+// import PropTypes from 'prop-types';
+// import { withTracker } from 'meteor/react-meteor-data';
+
+
+
+const App = () => (
+  <div>
+    <h1>This is our app!</h1>
+  </div>
+);
+
+export default App;
