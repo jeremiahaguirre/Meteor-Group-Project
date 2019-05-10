2776b2aae4b2afc3c5c1d11cb6cdffb1e1f83d79
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-10 11:25:04 -0700

    Create a test page to test components

---
+++ imports/ui/pages/Test/Test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Test = ({ classes }) => {
+  return (
+    <div>
+      <p>This is the Test page.Import whatever component you need to test inside this div</p>
+    </div>
+  );
+};
+
+export default Test;
