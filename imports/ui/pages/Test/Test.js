c690aec2e1fd3b8932f1b48312d877a797e21ce3
Saveen Toor <toorsaveen@gmail.com>
2019-05-10 11:58:10 -0700

    Merge component card branch.

--- imports/ui/pages/Test/Test.js
+++ imports/ui/pages/Test/Test.js
@@ -1,9 +1,14 @@
 import React from "react";
+import EmployeeCard from "../../components/EmployeeCard";
 
 const Test = ({ classes }) => {
   return (
     <div>
-      <p>This is the Test page.Import whatever component you need to test inside this div</p>
+      <p>
+        This is the Test page.Import whatever component you need to test inside
+        this div
+      </p>
+      <EmployeeCard />
     </div>
   );
 };
