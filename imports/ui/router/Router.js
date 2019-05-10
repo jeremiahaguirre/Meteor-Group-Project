ec7e05984ee003f220d1fc843c5489bea351e4f3
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-10 11:25:23 -0700

    Route to test page

--- imports/ui/router/Router.js
+++ imports/ui/router/Router.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import { Redirect, Route, Switch } from "react-router";
 import Welcome from "../pages/Welcome";
+import Test from "../pages/Test"
 
 export default () => (
   <div>
     <Switch>
       <Route path="/welcome" component={Welcome} />
+      <Route path="/test" component={Test}/>
       <Redirect to="/welcome" />
     </Switch>
   </div>
