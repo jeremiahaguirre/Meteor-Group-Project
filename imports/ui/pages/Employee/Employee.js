52f982af72f65c6e31d537e92009a347e2c9427f
Saveen Toor <toorsaveen@gmail.com>
2019-05-10 13:54:53 -0700

    Add modal skeleton.

--- imports/ui/pages/Employee/Employee.js
+++ imports/ui/pages/Employee/Employee.js
@@ -1,11 +1,82 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
 
-const Employee = ({ classes }) => {
-  return (
-    <div>
-      <p>This is the Employee page.</p>
-    </div>
-  );
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const styles = theme => ({
+  paper: {
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+    outline: 'none',
+  },
+});
+
+class SimpleModal extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div>
+        <Typography gutterBottom>Click to get the full Modal experience!</Typography>
+        <Button onClick={this.handleOpen}>Open Modal</Button>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={this.state.open}
+          onClose={this.handleClose}
+        >
+          <div style={getModalStyle()} className={classes.paper}>
+            <Typography variant="h6" id="modal-title">
+              Text in a modal
+            </Typography>
+            <Typography variant="subtitle1" id="simple-modal-description">
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+            <SimpleModalWrapped />
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+SimpleModal.propTypes = {
+  classes: PropTypes.object.isRequired,
 };
 
-export default Employee;
+// We need an intermediary variable for handling the recursive nesting.
+const SimpleModalWrapped = withStyles(styles)(SimpleModal);
+
+export default SimpleModalWrapped;
--- imports/ui/router/Router.js
+++ imports/ui/router/Router.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from "react";
 import { Redirect, Route, Switch } from "react-router";
 import Welcome from "../pages/Welcome";
-import Test from "../pages/Test"
+//import Test from "../pages/Test"
+import EmploeePage from "../pages/Employee";
 
 export default () => (
   <div>
     <Switch>
       <Route path="/welcome" component={Welcome} />
-      <Route path="/test" component={Test}/>
+      <Route path="/test" component={EmploeePage} />
       <Redirect to="/welcome" />
     </Switch>
   </div>
