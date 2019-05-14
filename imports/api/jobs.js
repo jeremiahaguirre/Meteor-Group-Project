108fe36356b4678c05b304de2cae6d9f1abaaec6
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-14 07:02:24 -0700

    Update jobs insert query to include more fields

--- imports/api/jobs.js
+++ imports/api/jobs.js
@@ -4,7 +4,7 @@ import { Meteor } from "meteor/meteor";
 export const Jobs = new Mongo.Collection("jobs");
 
 Meteor.methods({
-  "jobs.insert"(title, description, shift) {
+  "jobs.insert"(title, description, location, time, professions=[], workspaces=[]) {
     // if (!this.userId) {
     //   throw new Meteor.Error("not-authorized");
     // }
@@ -12,10 +12,10 @@ Meteor.methods({
     Jobs.insert({
       title,
       description,
-      location: undefined,
-      professions: [],
-      // workspace
-      shift,
+      location,
+      professions,
+      workspaces,
+      time,
       createdAt: new Date(),
       owner: this.userId // change when auth set up: (this.userId)
     });
