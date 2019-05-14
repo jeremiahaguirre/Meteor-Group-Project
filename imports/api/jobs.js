b58a790f817324356595584391900fed69607f39
Gavin Jin <gavinjin62@gmail.com>
2019-05-14 15:14:53 -0700

    Update job posting query

--- imports/api/jobs.js
+++ imports/api/jobs.js
@@ -4,7 +4,14 @@ import { Meteor } from "meteor/meteor";
 export const Jobs = new Mongo.Collection("jobs");
 
 Meteor.methods({
-  "jobs.insert"(title, description, location, time, professions=[], workspaces=[]) {
+  "jobs.insert"(
+    title,
+    description,
+    location,
+    time,
+    professions = [],
+    workspaces = []
+  ) {
     // if (!this.userId) {
     //   throw new Meteor.Error("not-authorized");
     // }
