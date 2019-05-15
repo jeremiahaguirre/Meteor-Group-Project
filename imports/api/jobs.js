3726794d8bcaf666e702bdd637c7fa4c50705288
Gavin Jin <gavinjin62@gmail.com>
2019-05-15 11:43:50 -0700

    job query workplace removal

--- imports/api/jobs.js
+++ imports/api/jobs.js
@@ -4,48 +4,28 @@ import { Meteor } from "meteor/meteor";
 export const Jobs = new Mongo.Collection("jobs");
 
 Meteor.methods({
-<<<<<<< HEAD
-  "jobs.insert"(
-=======
-  "jobs.open"(
->>>>>>> b36d22a1fe25545be5ac44c87f39b272d9b60e1b
-    title,
-    description,
-    location,
-    time,
-    professions = [],
-<<<<<<< HEAD
-    workspaces = []
-  ) {
-    // if (!this.userId) {
-    //   throw new Meteor.Error("not-authorized");
-    // }
-=======
-  ) {
+  "jobs.open"(title, description, location, time, professions = []) {
     if (!this.userId) {
       throw new Meteor.Error("not-authorized");
     }
->>>>>>> b36d22a1fe25545be5ac44c87f39b272d9b60e1b
 
     Jobs.insert({
       title,
       description,
       location,
-      professions,
       time,
+      professions,
       createdAt: new Date(),
       owner: this.userId,
-      taken: false,
+      taken: false
     });
   },
   "jobs.close"(_id) {
     if (!this.userId) {
       throw new Meteor.Error("not-authorized");
     }
-    Jobs.update({ _id:_id ,taken:false},
-      { $set: { taken:true } },
-    );
-  },
+    Jobs.update({ _id: _id, taken: false }, { $set: { taken: true } });
+  }
 });
 
 if (Meteor.isServer) {
@@ -53,10 +33,9 @@ if (Meteor.isServer) {
     return Jobs.find({});
   });
   Meteor.publish("openJobs", function openJobsPublication() {
-    return Jobs.find({taken:false});
+    return Jobs.find({ taken: false });
   });
   Meteor.publish("postedJobs", function postedJobsPublication() {
-    return Jobs.find({owner:this.userId});
+    return Jobs.find({ owner: this.userId });
   });
 }
-
