580ea06675fc2f5846422ef84ede4e4d89f60ae7
Jeremiah <happytobike@gmail.com>
2019-05-16 16:54:49 -0700

    Added larger background image

--- imports/ui/pages/Employer/styles.js
+++ imports/ui/pages/Employer/styles.js
@@ -1,5 +1,8 @@
 const styles = {
   main: {
+    backgroundImage: `url(${"/background.jpg"})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
     height: "100vh",
     margin: 0
   }
