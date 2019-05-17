d98647fa0cca820cddfa309e0801a247da8a1129
Jeremiah <happytobike@gmail.com>
2019-05-17 16:20:42 -0700

    Removed card background

--- imports/ui/components/RequestCard/styles.js
+++ imports/ui/components/RequestCard/styles.js
@@ -8,13 +8,8 @@ const styles = {
     width: "600px",
     height: "400px",
     overflowY: "scroll",
-    background: "#003550",
-    background:
-      "-moz-linear-gradient(top,  #003550 0%, #2989d8 50%, #003550 100%, #207cca 100%, #7db9e8 100%)d",
-    background:
-      "-webkit-linear-gradient(top,  #003550 0%,#2989d8 50%,#003550 100%,#207cca 100%,#7db9e8 100%)",
-    background:
-      "linear-gradient(to bottom,  #003550 0%,#2989d8 50%,#003550 100%,#207cca 100%,#7db9e8 100%)",
+    background: "rgba(0,0,0,0.0)",
+    boxShadow: "none",
     borderRadius: "10px"
   },
   h2: {
