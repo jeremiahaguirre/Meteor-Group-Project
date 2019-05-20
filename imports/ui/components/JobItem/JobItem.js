1c69d41e5e7647ef71eeed3a90b5d81e6ba8236f
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-20 10:04:40 -0700

    Refactor list item into a component

---
+++ imports/ui/components/JobItem/JobItem.js
@@ -0,0 +1,57 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import Gravatar from "react-gravatar";
+import moment from "moment";
+import styles from "./styles";
+
+const JobItem = ({ classes, job }) => {
+    return (
+    <div className={classes.root} key={job._id}>
+        <ListItem className={classes.list} alignItems="flex-start">
+        <ListItemAvatar>
+            <Avatar>
+            <Gravatar
+                email={job.owner ? job.owner.emails[0].address : ""}
+            />
+            </Avatar>
+        </ListItemAvatar>
+        <ListItemText
+            className={classes.h1}
+            primary={job.title}
+            secondary={
+            <React.Fragment>
+                <Typography component="span" color="textPrimary">
+                Description: {job.description}{" "}
+                </Typography>{" "}
+                <Typography component="span" color="textPrimary">
+                Date:{" "}
+                {moment(job.createdAt)
+                    .add(10, "days")
+                    .calendar()}
+                </Typography>{" "}
+                <Typography component="span" color="textPrimary">
+                Requierments: {job.professions.join(", ")}
+                </Typography>{" "}
+                <Typography component="span" color="textPrimary">
+                Location: {job.location}
+                </Typography>
+            </React.Fragment>
+            }
+        />
+        </ListItem>
+    </div>
+    );
+}
+
+JobItem.propTypes = {
+    classes: PropTypes.object.isRequired,
+    job: PropTypes.object.isRequired
+  };
+  
+  export default (withStyles(styles)(JobItem));
--- imports/ui/components/JobsList/JobsList.js
+++ imports/ui/components/JobsList/JobsList.js
@@ -3,23 +3,17 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import Card from "@material-ui/core/Card";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
 import { withTracker } from "meteor/react-meteor-data";
-import Divider from "@material-ui/core/Divider";
-import Gravatar from "react-gravatar";
-import moment from "moment";
 import styles from "./styles";
 import { getJobPosts } from "../../helpers/functions";
+import JobItem from '../JobItem';
 
 const JobsList = ({ classes, filter, jobs }) => {
   return (
     <div>
       <Typography className={classes.h2} component="h2">
-        All Jobs
+        Job Posts
       </Typography>
       <Card className={classes.card}>
         <List>
@@ -29,40 +23,7 @@ const JobsList = ({ classes, filter, jobs }) => {
             )
             .map(job => {
               return (
-                <div className={classes.root} key={job._id}>
-                  <ListItem className={classes.list} alignItems="flex-start">
-                    <ListItemAvatar>
-                      <Avatar>
-                        <Gravatar
-                          email={job.owner ? job.owner.emails[0].address : ""}
-                        />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText
-                      className={classes.h1}
-                      primary={job.title}
-                      secondary={
-                        <React.Fragment>
-                          <Typography component="span" color="textPrimary">
-                            Description: {job.description}{" "}
-                          </Typography>{" "}
-                          <Typography component="span" color="textPrimary">
-                            Date:{" "}
-                            {moment(job.createdAt)
-                              .add(10, "days")
-                              .calendar()}
-                          </Typography>{" "}
-                          <Typography component="span" color="textPrimary">
-                            Requierments: {job.professions.join(", ")}
-                          </Typography>{" "}
-                          <Typography component="span" color="textPrimary">
-                            Location: {job.location}
-                          </Typography>
-                        </React.Fragment>
-                      }
-                    />
-                  </ListItem>
-                </div>
+                <JobItem job={job}/>
               );
             })}
         </List>
