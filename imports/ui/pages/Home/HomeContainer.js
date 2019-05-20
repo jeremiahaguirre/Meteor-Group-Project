fee3cff0ee8c878d9183e528324b3e82501bc3a5
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-20 09:33:29 -0700

    Rework home page

---
+++ imports/ui/pages/Home/HomeContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Home from "./Home";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles";
+
+class HomeContainer extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.main}>
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(HomeContainer);
---
+++ imports/ui/pages/Home/styles.js
@@ -0,0 +1,9 @@
+const styles = {
+  main: {
+    background: "rgb(231,234,237)",
+    height: "100vh",
+    margin: 0
+  }
+};
+
+export default styles;
